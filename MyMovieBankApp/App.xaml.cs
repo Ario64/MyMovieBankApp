ef910d919906dc9cf4a385ab2634782114bf46c9
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2022-08-05 16:19:02 +0430

    Add project files.

---
+++ MyMovieBankApp/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Windows;
+using MovieWpfApp.Utility;
+
+namespace MyMovieBankApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            PersianCulture culture = new PersianCulture();
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
---
+++ MyMovieBankApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace MyMovieBankApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Thread.Sleep(3000);
+        }
+
+        //close button-----
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        //minimize button-----
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        //scroll towards left button
+        private void BtnMoveLeft_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //scroll towards right button
+        private void BtnMoveRight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //draging window
+        private void RectanlForDrag_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+            else if (e.MiddleButton == MouseButtonState.Released)
+            {
+                this.WindowState =
+                    this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            }
+        }
+    }
+}
